3645e9eb7d27f0ba2e0929649cf32cb3c022b669
José Martínez <jgermanmm@gmail.com>
2026-03-04 12:59:31 -0300

    conversion

---
+++ 01-Intro/js/conversion.js
@@ -0,0 +1,18 @@
+
+
+let nombre = "Alberto";
+
+console.log(typeof nombre)
+
+nombre = 45
+
+console.log(typeof nombre)
+
+console.log(5 + "5")
+console.log("22" == 22) // comparacion simple
+console.log("22" === 22) // comparación estricta
+
+console.log("5"+ "40")
+console.log(Number("5") + Number("40"))
+
+console.log(1 + true)
