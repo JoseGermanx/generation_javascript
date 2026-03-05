49ff55dff4945043d95174a95de5db0050f9e508
José Martínez <jgermanmm@gmail.com>
2026-03-05 12:24:57 -0300

    try catch

--- 02-condicionales/js/ejercicio.js
+++ 02-condicionales/js/ejercicio.js
@@ -46,10 +46,52 @@ botonDia.addEventListener("click", function(){
             resultadoDia.textContent = "No es un día válido."
             break;
     }
+})
 
+
+// Cálculo de la raíz
+const inputNumero = document.getElementById("numero");
+const botonCalcular = document.getElementById("btnCalcular");
+const resultadoRaiz = document.getElementById("resultadoRaiz");
+
+botonCalcular.addEventListener("click", function(){
+
+try {
+     const valor = inputNumero.value;
+
+     const numero = Number(valor);
+
+     if (valor === ""){
+        throw new Error("Deber ingresar un valor.")
+     }
+
+     if (isNaN(numero)) {
+        throw new Error("Debe ingresar un número válido.")
+     }
+
+     if (numero < 0) {
+        throw new Error("Debes ingresar un número positivo.")
+     }
+
+
+    const raiz = Math.sqrt(numero)
+
+    resultadoRaiz.textContent = `La raíz cuadrada de ${numero} es: ${raiz}`
+
+} catch (error) {
+
+    resultadoRaiz.textContent = `Error: ${error.message}  `
+    
+}
+   
 
 
 })
+
+
+
+
+
 
 
 
