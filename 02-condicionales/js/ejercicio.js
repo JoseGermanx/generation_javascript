cba55daf225b774fdab4c5f5052c37e1862fe860
José Martínez <jgermanmm@gmail.com>
2026-03-05 12:03:25 -0300

    dia se la semana

--- 02-condicionales/js/ejercicio.js
+++ 02-condicionales/js/ejercicio.js
@@ -17,3 +17,40 @@ if (edad === "") {
     resultado.textContent = "Eres menor edad."
 }
 })
+
+
+// Día de la semana
+const selecDia = document.getElementById("dia");
+const botonDia = document.getElementById("btnDia");
+const resultadoDia = document.getElementById("resultadoDia");
+
+
+botonDia.addEventListener("click", function(){
+
+
+    const dia = selecDia.value;
+
+    switch (dia) {
+        case "lunes":
+        case "martes":
+        case "miércoles":
+        case "jueves":
+        case "viernes":
+            resultadoDia.textContent = "Es día laborable."
+            break;
+        case "sabado":
+        case "domingo":
+            resultadoDia.textContent = "Es fin de semana."
+            break;
+        default:
+            resultadoDia.textContent = "No es un día válido."
+            break;
+    }
+
+
+
+})
+
+
+
+
