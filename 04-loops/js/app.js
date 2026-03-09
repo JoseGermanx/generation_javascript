b3b399c04e4dde5790abaa33def6fbcbcbee8c5a
José Martínez <jgermanmm@gmail.com>
2026-03-09 10:39:43 -0300

    Add initial HTML structure and FizzBuzz JavaScript implementation

---
+++ 04-loops/js/app.js
@@ -0,0 +1,27 @@
+
+
+
+// FizzBuzz
+// Juego matemático --> contar números de manera especial
+// 123456789
+// Si un número es múltiplo de 3 --> Fizz
+// Si un número es múltiplo de 5 --> Buzz
+// Si un número es múltiplo 3 y de 5 --> FizzBuzz
+
+// 1 2 Fizz 4 Buzz Fizz 7 8 Buzz
+
+// == comparador sin tomar en consideración el tipo de dato
+// === comparador /estricto) que si toma en consideración el tipo de dato
+
+for (let i = 1; i <= 1000; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz")
+    } else if (i % 3 === 0) {
+        console.log("Fizz")
+    }
+    else if (i % 5 === 0) {
+        console.log("Buzz")
+    } else {
+        console.log(i)
+    }
+}
