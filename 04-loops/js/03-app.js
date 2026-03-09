b80069b22fdb069cc2c66cbf1baec5cb00590df5
José Martínez <jgermanmm@gmail.com>
2026-03-09 11:21:08 -0300

    continue y break

---
+++ 04-loops/js/03-app.js
@@ -0,0 +1,29 @@
+
+
+// continue
+
+for (let i = 0; i <= 5; i++) {
+
+    console.log("Interación: " + i)
+
+    if(i === 3){
+        continue;
+    }
+
+    console.log("Valor: " + i)
+
+}
+
+// break --Búsqueda de un valor
+
+for (let i = 0; i <= 5; i++) {
+
+    console.log("Interación: " + i)
+
+    if(i === 1){
+        break;
+    }
+
+    console.log("Valor: " + i)
+
+}
