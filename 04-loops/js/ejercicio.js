d2f73470bbe3cfc8ff10efe23157ee3cc3d949d2
José Martínez <jgermanmm@gmail.com>
2026-03-09 11:49:13 -0300

    Add initial structure for ejercicio.js with button descriptions for number display and counting

---
+++ 04-loops/js/ejercicio.js
@@ -0,0 +1,21 @@
+
+
+// Crear un botón "Mostrar números" que al hacer clic muestre en pantalla los números del 1 al 10.
+
+// Cada número debe aparecer dentro de un <li> dentro de una lista <ul>.
+
+
+
+
+
+
+// Crear un botón "Contar hasta 5".
+
+// Cuando el usuario haga clic, mostrar en un <p> los números 1 2 3 4 5 usando un ciclo while.
+
+
+
+
+// Crear un botón "Contar hasta 5".
+
+// Cuando el usuario haga clic, mostrar en un <p> los números 1 2 3 4 5 usando un ciclo while.
