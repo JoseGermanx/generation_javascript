a3e5620d07c7bb3a0263a43821ff9c3011a2e345
José Martínez <jgermanmm@gmail.com>
2026-03-16 12:35:26 -0300

    ejemplo didactico pass login

--- 07-objetos/js/06-app.js
+++ 07-objetos/js/06-app.js
@@ -2,3 +2,36 @@
 console.log("Hola mundo . Ejemplo password");
 
 
+class User {
+    #password
+
+    constructor(nombre, email, password){
+        this.nombre = nombre;
+        this.email = email;
+        this.#password = password;
+    }
+
+    saludar(){
+        `Hola soy ${this.nombre}!`;
+    };
+
+    cambiarPassword(nuevaPassword){
+        this.#password = nuevaPassword;
+    }
+
+    validarPassword(password){
+        return this.#password === password;
+    };
+
+};
+
+const usuario = new User("Luisa", "luisa@luisa.com", "123456");
+
+const login = prompt("Ingresa tu contraseña");
+
+if (usuario.validarPassword(login)){
+    console.log("Bienvenido al sistema, ingreso correcto.")
+} else {
+    console.error("Hubo un error, contraseña inválida!!")
+}
+
