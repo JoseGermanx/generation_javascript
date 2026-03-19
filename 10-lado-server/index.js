900a47509b85aa3af196b6e818917be9d2e82f0e
José Martínez <jgermanmm@gmail.com>
2026-03-19 12:57:02 -0300

    node

---
+++ 10-lado-server/index.js
@@ -0,0 +1,13 @@
+
+// commonjs
+const { add, multiply } = require('./js.js')
+
+let texto = "Hola mundo desde node"
+
+console.log(texto)
+
+console.log(add(5,5))
+
+console.log(multiply(5,5))
+
+console.log("Hola mundo, bienvenidos al backend!!! ")
